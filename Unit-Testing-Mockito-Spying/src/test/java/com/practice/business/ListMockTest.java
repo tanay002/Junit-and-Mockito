e861f5eb3b9db2dbb0e3e01e458b93b4098bd882
Tanay <tanay.saxena002@gmail.com>
2023-04-25 22:11:23 +0530

    verify

---
+++ Unit-Testing-Mockito-Spying/src/test/java/com/practice/business/ListMockTest.java
@@ -0,0 +1,65 @@
+package com.practice.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.atMost;
+import static org.mockito.Mockito.never;
+
+class ListMockTest {
+
+	List<String> mock = mock(List.class);
+
+	@Test
+	public void size_basic() {
+		when(mock.size()).thenReturn(5);
+		assertEquals(5, mock.size());
+	}
+
+	@Test
+	public void returnDifferentValues() {
+		when(mock.size()).thenReturn(5).thenReturn(10);
+		assertEquals(5, mock.size());
+		assertEquals(10, mock.size());
+	}
+
+	@Test
+	public void returnWithParameters() {
+		when(mock.get(0)).thenReturn("practices");
+		assertEquals("practices", mock.get(0));
+		assertEquals(null, mock.get(1));
+	}
+	
+	@Test
+	public void returnWithAnyParameters() {
+		when(mock.get(anyInt())).thenReturn("Practice_junit");
+		assertEquals("Practice_junit", mock.get(0));
+		assertEquals("Practice_junit", mock.get(1));
+	}
+	
+	@Test
+	public void verificationBasics() {
+		// SUT
+		String value1 = mock.get(0);
+		String value2 = mock.get(1);
+
+		// Verify
+		verify(mock).get(0);
+		verify(mock, times(2)).get(anyInt());
+		verify(mock, atLeast(1)).get(anyInt());
+		verify(mock, atLeastOnce()).get(anyInt());
+		verify(mock, atMost(2)).get(anyInt());
+		verify(mock, never()).get(2);
+	}
+}
