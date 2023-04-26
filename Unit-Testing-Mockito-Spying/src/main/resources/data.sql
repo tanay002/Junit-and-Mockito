25397d91f006c05b94648c8678274dfbd4303b46
Tanay <tanay.saxena002@gmail.com>
2023-04-26 22:59:55 +0530

    Intergration Testing 1

--- Unit-Testing-Mockito-Spying/src/main/resources/data.sql
+++ Unit-Testing-Mockito-Spying/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-insert into item (id,name,price,quantity) values (1,'Banana',40,12); 
+--insert into item (id,name,price,quantity) values (1,'Banana',40,12); 
 insert into item (id,name,price,quantity) values (2,'Apple',110,6); 
 insert into item (id,name,price,quantity) values (3,'Orange',50,6); 
-insert into item (id,name,price,quantity) values (4,'Grapes',45,1); 
-insert into item (id,name,price,quantity) values (5,'Papaya',30,1); 
-insert into item (id,name,price,quantity) values (6,'Mango',95,3); 
+-- insert into item (id,name,price,quantity) values (4,'Grapes',45,1); 
+-- insert into item (id,name,price,quantity) values (5,'Papaya',30,1); 
+-- insert into item (id,name,price,quantity) values (6,'Mango',95,3); 
---
+++ Unit-Testing-Mockito-Spying/src/test/java/com/practice/controller/ItemControllerIntegrationTesting.java
@@ -0,0 +1,25 @@
+package com.practice.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ItemControllerIntegrationTesting {
+
+	@Autowired
+	TestRestTemplate testRestTemplate;
+	
+	@Test
+	public void contextLoads() throws JSONException
+	{
+		String response=this.testRestTemplate.getForObject("/all-item-from-database", String.class);
+		JSONAssert.assertEquals("[{id:2},{id:3}]", response,false);
+	}
+}
